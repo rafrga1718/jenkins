8b5efdfa635b12f760a108ce1fb3979c22327976
rafrga1718 <raul@raul-VirtualBox>
2018-02-09 19:47:24 +0100

    Modificació 1 de la prova d'IC amb Jenkins

--- src/jenkins/Jenkins.java
+++ src/jenkins/Jenkins.java
@@ -27,6 +27,7 @@ public class Jenkins {
             bw.write("  </body>");bw.newLine();
             bw.write("</html>");bw.newLine();
             bw.close();
+            bw.write("    Modificació del projecte jenkins");bw.newLine();
         }
     }
 }
